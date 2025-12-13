6ed9d780748d59a6cea53ea97879359fa58e55a6
Aleksandr Khristiukhin <ak@robosl.one>
2025-12-13 16:24:45 +0000

    added loader to habit icon

--- src/components/habit/icon.tsx
+++ src/components/habit/icon.tsx
@@ -2,13 +2,17 @@ import { useHabitContext } from "@/components/habit/context"
 import * as colors from "@/lib/colors"
 import * as icons from "@/lib/icons"
 import { cn } from "@/lib/utils"
-import { type LucideProps } from "lucide-react"
+import { Loader2, type LucideProps } from "lucide-react"
 import type React from "react"
 
 export const HabitIcon: React.FC<LucideProps> = (props) => {
-    const { habit } = useHabitContext()
-
+    const { habit, isFetching } = useHabitContext()
     const color = colors.index[colors.fromString(habit.color)]
+
+    if (isFetching) {
+        return <Loader2 {...props} className={cn("animate-spin", color.text, props.className)} />
+    }
+
     return icons.render(habit.icon, {
         ...props,
         className: cn(color.text, props.className),
